f927ca7de2317f259b4c3ab01fa528ac58705ae0
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2024-06-07 17:13:56 +0700

    change firebase cert name

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 
 const setupFirestore = () => {
     try {
-        const serviceAccount = require('./klock-firebase-firebase-adminsdk-liqsc-4b5c6c13c6.json');
+        const serviceAccount = require('./klock-firebase-cert.json');
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
             storageBucket: 'gs://klock-firebase.appspot.com'
